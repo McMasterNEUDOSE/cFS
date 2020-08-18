94a7bdbd785c8be74fc13fff425e553cbe4c2f6b
John Barberiz <barberizja@gmail.com>
2020-08-17 21:06:22 -0400

    Task: Porting PSP - Stubbed cfe_psp_support.c
    [Why]
    Stubbing linux directories in PSP to use as a starting
    point for building FreeRTOS
    
    [How]
    Stubbed cfe_psp_support.c

--- psp/fsw/pc-linux/src/cfe_psp_support.c
+++ psp/fsw/pc-linux/src/cfe_psp_support.c
@@ -33,7 +33,6 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 #include "common_types.h"
@@ -43,15 +42,12 @@
 ** Types and prototypes for this module
 */
 #include "cfe_psp.h"
-#include "cfe_psp_config.h"
 #include "cfe_psp_memory.h"
 
 
 /*
 ** External Variables
 */
-extern uint32  CFE_PSP_SpacecraftId;
-extern uint32  CFE_PSP_CpuId;
 
 
 /******************************************************************************
@@ -69,52 +65,6 @@ extern uint32  CFE_PSP_CpuId;
 
 void CFE_PSP_Restart(uint32 reset_type)
 {
-   if ( reset_type == CFE_PSP_RST_TYPE_POWERON )
-   {
-       OS_printf("CFE_PSP: Exiting cFE with POWERON Reset status.\n");
-
-       /* Also delete the SHM segments, so they will be recreated on next boot */
-       /* Deleting these memories will unlink them, but active references should still work */
-       CFE_PSP_DeleteProcessorReservedMemory();
-   }
-   else
-   {
-       OS_printf("CFE_PSP: Exiting cFE with PROCESSOR Reset status.\n");
-   }
-
-   /*
-    * Record the reset type for the next boot.
-    */
-   CFE_PSP_ReservedMemoryMap.BootPtr->NextResetType = reset_type;
-   CFE_PSP_ReservedMemoryMap.BootPtr->ValidityFlag = CFE_PSP_BOOTRECORD_VALID;
-
-   /*
-    * Begin process of orderly shutdown.
-    *
-    * This should cause the original thread (main task) to wake up
-    * and start the shutdown procedure.
-    */
-   CFE_PSP_IdleTaskState.ShutdownReq = true;
-   pthread_kill(CFE_PSP_IdleTaskState.ThreadID, CFE_PSP_EXCEPTION_EVENT_SIGNAL);
-
-   /*
-    * Give time for the orderly shutdown to occur.
-    *
-    * Normally during shutdown this task will be deleted, and therefore
-    * this does not return.
-    *
-    * However, if problems occur (e.g. deadlock) eventually this timeout
-    * will expire and return.
-    */
-   OS_TaskDelay(CFE_PSP_RESTART_DELAY);
-
-   /*
-    * Timeout expired without this task being deleted, so abort().
-    *
-    * This should generate a core file to reveal what went wrong
-    * with normal shutdown.
-    */
-   abort();
 
 }
 
@@ -134,9 +84,7 @@ void CFE_PSP_Restart(uint32 reset_type)
 
 void CFE_PSP_Panic(int32 ErrorCode)
 {
-   OS_printf("CFE_PSP_Panic Called with error code = 0x%08X. Exiting.\n",(unsigned int)ErrorCode);
-   OS_printf("The cFE could not start.\n");
-   abort(); /* abort() is preferable to exit(-1), as it may create a core file for debug */
+
 }
 
 
@@ -155,7 +103,7 @@ void CFE_PSP_Panic(int32 ErrorCode)
 */
 void CFE_PSP_FlushCaches(uint32 type, void* address, uint32 size)
 {
-   printf("CFE_PSP_FlushCaches called -- Currently no Linux/OSX/Cygwin implementation\n");
+
 }
 
 /*
@@ -177,7 +125,7 @@ void CFE_PSP_FlushCaches(uint32 type, void* address, uint32 size)
 */
 uint32 CFE_PSP_GetProcessorId    (void)
 {
-    return(CFE_PSP_CpuId);
+    return 0;
 }
 
 
@@ -198,5 +146,5 @@ uint32 CFE_PSP_GetProcessorId    (void)
 */
 uint32 CFE_PSP_GetSpacecraftId   (void)
 {
-   return(CFE_PSP_SpacecraftId);
+   return 0;
 }
