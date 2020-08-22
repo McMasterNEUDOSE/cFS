3078d2bdf1cbfc85cdf7d91c3fee85e90e2a6e20
John Barberiz <barberizja@gmail.com>
2020-08-22 11:06:36 -0400

    ask: Porting OSAL - Stubbed/cleaned up os-impl-tasks.c
    [Why]
    os-impl-tasks.c still had dependencies on linux, and didn't
    return errors for unimplemented functions
    
    [How]
    Removed dependencies, fixed return values

--- osal/src/os/posix/src/os-impl-tasks.c
+++ osal/src/os/posix/src/os-impl-tasks.c
@@ -20,7 +20,7 @@
 
 /**
  * \file     os-impl-tasks.c
- * \ingroup  posix
+ * \ingroup  FreeRTOS
  * \author   joseph.p.hickey@nasa.gov
  *
  */
@@ -30,20 +30,11 @@
  ***************************************************************************************/
 
 #include "os-posix.h"
-#include "bsp-impl.h"
-#include <sched.h>
 
 #include "os-impl-tasks.h"
 
 #include "os-shared-task.h"
 #include "os-shared-idmap.h"
-
-/*
- * Defines
- */
-#ifndef PTHREAD_STACK_MIN
-#define PTHREAD_STACK_MIN   (8*1024)
-#endif
 
 /* Tables where the OS object information is stored */
 OS_impl_task_internal_record_t      OS_impl_task_table          [OS_MAX_TASKS];
@@ -63,129 +54,7 @@ OS_impl_task_internal_record_t      OS_impl_task_table          [OS_MAX_TASKS];
  ----------------------------------------------------------------------------------------*/
 int32 OS_Posix_TaskAPI_Impl_Init(void)
 {
-
-
-   /* Initialize Local Tables */
-
-
-   /* Clear the "limits" structs otherwise the compiler may warn
-    * about possibly being used uninitialized (false warning)
-    */
-
-
-   /*
-    * Create the key used to store OSAL task IDs
-    */
-
-   /*
-   ** Disable Signals to parent thread and therefore all
-   ** child threads create will block all signals
-   ** Note: Timers will not work in the application unless
-   **       threads are spawned in OS_Application_Startup.
-   */
-
-   /*
-    * Keep these signals unblocked so the process can be interrupted
-    */
-
-   /*
-    * One should not typically block ANY of the synchronous error
-    * signals, i.e. SIGSEGV, SIGFPE, SIGILL, SIGBUS
-    *
-    * The kernel generates these signals in response to hardware events
-    * and they get routed to the _specific thread_ that was executing when
-    * the problem occurred.
-    *
-    * While it is technically possible to block these signals, the result is
-    * undefined, and it makes debugging _REALLY_ hard.  If the kernel ever does
-    * send one it means there really is a major problem, best to listen to it,
-    * and not ignore it.
-    */
-
-   /*
-    * Set the mask and store the original (default) mask in the POSIX_GlobalVars.NormalSigMask
-    */
-
-
-   /*
-    * Add all "RT" signals into the POSIX_GlobalVars.NormalSigMask
-    * This will be used for the signal mask of the main thread
-    * (This way it will end up as the default/original signal mask plus all RT sigs)
-    */
-
-
-   /*
-    * SIGHUP is used to wake up the main thread when necessary,
-    * so make sure it is NOT in the set.
-    */
-
-   /*
-   ** Install noop as the signal handler for SIGUP.
-   */
-
-   /*
-   ** Raise the priority of the current (main) thread so that subsequent
-   ** application initialization will complete.  This had previously been
-   ** done by the BSP and but it is moved here.
-   **
-   ** This will only work if the user owning this process has permission
-   ** to create real time threads.  Otherwise, the default priority will
-   ** be retained.  Typically this is only the root user, but finer grained
-   ** permission controls are out there.  So if it works, great, but if
-   ** a permission denied error is generated, that is OK too - this allows
-   ** easily debugging code as a normal user.
-   */
-
-           /*
-            * If both policies are valid, choose the best. In general, FIFO is preferred
-            * since it is simpler.
-            *
-            * But, RR is preferred if mapping several OSAL priority levels into the
-            * same local priority level. For instance, if 2 OSAL tasks are created at priorities
-            * "2" and "1", both may get mapped to local priority 98, and if using FIFO then the
-            * task at priority "2" could run indefinitely, never letting priority "1" execute.
-            *
-            * This violates the original intent, which would be to have priority "1" preempt
-            * priority "2" tasks.  RR is less bad since it at least guarantees both tasks some
-            * CPU time,
-            */
-
-               /*
-                * If the spread from min->max is greater than what OSAL actually needs,
-                * then FIFO is the preferred scheduler.  Must take into account one extra level
-                * for the root task.
-                */
-
-
-           /*
-            * This OSAL POSIX implementation will reserve the absolute highest priority
-            * for the root thread, which ultimately will just pend in sigsuspend() so
-            * it will not actually DO anything, except if sent a signal.  This way,
-            * that thread will still be able to preempt a high-priority user thread that
-            * has gone awry (i.e. using 100% cpu in FIFO mode).
-            */
-
-
-           /*
-            * If the spread from min->max is greater than what OSAL actually needs,
-            * then truncate it at the number of OSAL priorities.  This will end up mapping 1:1.
-            * and leaving the highest priority numbers unused.
-            */
-
-
-           /*
-            * Set the boolean to indicate that "setschedparam" worked --
-            * This means that it is also expected to work for future calls.
-            */
-
-
-   /*
-    * In strict (non-permissive) mode, if the task priority setting did not work, fail with an error.
-    * This would be used on a real target where it needs to be ensured that priorities are active
-    * and the "silent fallback" of debug mode operation is not desired.
-    */
-
-   return OS_SUCCESS;
+   return OS_ERR_NOT_IMPLEMENTED;
 } /* end OS_Posix_TaskAPI_Impl_Init */
 
 
@@ -198,39 +67,7 @@ int32 OS_Posix_TaskAPI_Impl_Init(void)
  *-----------------------------------------------------------------*/
 int32 OS_Posix_InternalTaskCreate_Impl(pthread_t *pthr, uint32 priority, size_t stacksz, PthreadFuncPtr_t entry, void *entry_arg)
 {
-
-
-
-    /*
-     ** Initialize the pthread_attr structure.
-     ** The structure is used to set the stack and priority
-     */
-
-    /*
-     * Adjust the stack size parameter.
-     *
-     * POSIX has additional restrictions/limitations on the stack size of tasks that
-     * other RTOS environments may not have.  Specifically POSIX says that the stack
-     * size must be at least PTHREAD_STACK_MIN and may also need to be a multiple of the
-     * system page size.
-     *
-     * Rounding up means the user might get a bigger stack than they requested, but
-     * that should not break anything aside from consuming extra memory.
-     */
-
-
-    /*
-    ** Set the Stack Size
-    */
-
-
-    /*
-    ** Test to see if the original main task scheduling priority worked.
-    ** If so, then also set the attributes for this task.  Otherwise attributes
-    ** are left at default.
-    */
-
-    return OS_SUCCESS;
+    return OS_ERR_NOT_IMPLEMENTED;
 } /* end OS_Posix_InternalTaskCreate_Impl */
 
 
@@ -244,7 +81,7 @@ int32 OS_Posix_InternalTaskCreate_Impl(pthread_t *pthr, uint32 priority, size_t 
  *-----------------------------------------------------------------*/
 int32 OS_TaskCreate_Impl (uint32 task_id, uint32 flags)
 {
-   return OS_SUCCESS;
+   return OS_ERR_NOT_IMPLEMENTED;
 } /* end OS_TaskCreate_Impl */
 
 
@@ -258,8 +95,7 @@ int32 OS_TaskCreate_Impl (uint32 task_id, uint32 flags)
  *-----------------------------------------------------------------*/
 int32 OS_TaskMatch_Impl(uint32 task_id)
 {
-
-   return OS_SUCCESS;
+   return OS_ERR_NOT_IMPLEMENTED;
 } /* end OS_TaskMatch_Impl */
 
 
@@ -273,14 +109,7 @@ int32 OS_TaskMatch_Impl(uint32 task_id)
  *-----------------------------------------------------------------*/
 int32 OS_TaskDelete_Impl (uint32 task_id)
 {
-    /*
-    ** Try to delete the task
-    ** If this fails, not much recourse - the only potential cause of failure
-    ** to cancel here is that the thread ID is invalid because it already exited itself,
-    ** and if that is true there is nothing wrong - everything is OK to continue normally.
-    */
-    return OS_SUCCESS;
-
+    return OS_ERR_NOT_IMPLEMENTED;
 } /* end OS_TaskDelete_Impl */
 
 
@@ -308,8 +137,7 @@ void OS_TaskExit_Impl()
  *-----------------------------------------------------------------*/
 int32 OS_TaskDelay_Impl(uint32 millisecond)
 {
-
-  return OS_SUCCESS;
+  return OS_ERR_NOT_IMPLEMENTED;
 } /* end OS_TaskDelay_Impl */
 
 
@@ -323,8 +151,7 @@ int32 OS_TaskDelay_Impl(uint32 millisecond)
  *-----------------------------------------------------------------*/
 int32 OS_TaskSetPriority_Impl (uint32 task_id, uint32 new_priority)
 {
-
-   return OS_SUCCESS;
+   return OS_ERR_NOT_IMPLEMENTED;
 } /* end OS_TaskSetPriority_Impl */
 
 
@@ -338,8 +165,7 @@ int32 OS_TaskSetPriority_Impl (uint32 task_id, uint32 new_priority)
  *-----------------------------------------------------------------*/
 int32 OS_TaskRegister_Impl(uint32 global_task_id)
 {
-
-   return OS_SUCCESS;
+   return OS_ERR_NOT_IMPLEMENTED;
 } /* end OS_TaskRegister_Impl */
 
 
@@ -367,16 +193,7 @@ uint32 OS_TaskGetId_Impl (void)
  *-----------------------------------------------------------------*/
 int32 OS_TaskGetInfo_Impl (uint32 task_id, OS_task_prop_t *task_prop)
 {
-   /*
-    * NOTE - this is not really valid, as you can't officially
-    * cast a pthread_t to an integer
-    * (in fact this fails on cygwin where pthread_t is NOT an integral type)
-    *
-    * This is just a hack to fill the value with something.
-    * To be portable, the application should _NOT_ be using OStask_id for anything.
-    */
-
-   return OS_SUCCESS;
+   return OS_ERR_NOT_IMPLEMENTED;
 } /* end OS_TaskGetInfo_Impl */
 
 
@@ -390,7 +207,7 @@ int32 OS_TaskGetInfo_Impl (uint32 task_id, OS_task_prop_t *task_prop)
  *-----------------------------------------------------------------*/
 bool OS_TaskIdMatchSystemData_Impl(void *ref, uint32 local_id, const OS_common_record_t *obj)
 {
-    return true;
+    return false;
 }
 
 /*----------------------------------------------------------------
@@ -403,8 +220,7 @@ bool OS_TaskIdMatchSystemData_Impl(void *ref, uint32 local_id, const OS_common_r
  *-----------------------------------------------------------------*/
 int32 OS_TaskValidateSystemData_Impl(const void *sysdata, uint32 sysdata_size)
 {
-
-    return OS_SUCCESS;
+    return OS_ERR_NOT_IMPLEMENTED;
 }
 
 
