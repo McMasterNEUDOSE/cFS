312d84719611a8127cd1250dc5da7bee4250f844
John Barberiz <barberizja@gmail.com>
2020-08-22 10:35:05 -0400

    ask: Porting OSAL - Stubbed/cleaned up os-impl-errors.c
    [Why]
    os-impl-errors.c still had dependencies on linux, and didn't
    return errors for unimplemented functions
    
    [How]
    Removed dependencies, fixed return values

--- osal/src/os/posix/src/os-impl-errors.c
+++ osal/src/os/posix/src/os-impl-errors.c
@@ -29,8 +29,6 @@
                                     INCLUDE FILES
  ***************************************************************************************/
 
-#include "os-posix.h"
-
 #include "os-shared-errors.h"
 
 const OS_ErrorTable_Entry_t OS_IMPL_ERROR_NAME_TABLE[] = { { 0, NULL } };
