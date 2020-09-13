2e3f462528ad7d7fe880250bd8791e6700c9d8fb
Stephen <stephen99scott@gmail.com>
2020-09-12 23:01:28 -0400

    Added CSP ERR EID macros

--- apps/csp_app/fsw/src/csp_app_events.h
+++ apps/csp_app/fsw/src/csp_app_events.h
@@ -1,44 +1,34 @@
 /************************************************************************
 **
-**      GSC-18128-1, "Core Flight Executive Version 6.7"
-**
-**      Copyright (c) 2006-2019 United States Government as represented by
-**      the Administrator of the National Aeronautics and Space Administration.
-**      All Rights Reserved.
-**
-**      Licensed under the Apache License, Version 2.0 (the "License");
-**      you may not use this file except in compliance with the License.
-**      You may obtain a copy of the License at
-**
-**        http://www.apache.org/licenses/LICENSE-2.0
-**
-**      Unless required by applicable law or agreed to in writing, software
-**      distributed under the License is distributed on an "AS IS" BASIS,
-**      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-**      See the License for the specific language governing permissions and
-**      limitations under the License.
-**
 ** File: csp_app_events.h
 **
 ** Purpose:
 **  Define CSP App Events IDs
 **
-** Notes:
+** Author:
+**   Stephen Scott
 **
 *************************************************************************/
 #ifndef _csp_app_events_h_
 #define _csp_app_events_h_
 
 
-#define CSP_RESERVED_EID                   0
-#define CSP_STARTUP_INF_EID                1
-#define CSP_COMMAND_ERR_EID                2
-#define CSP_COMMANDNOP_INF_EID             3
-#define CSP_COMMANDRST_INF_EID             4
-#define CSP_INVALID_MSGID_ERR_EID          5
-#define CSP_LEN_ERR_EID                    6
-#define CSP_PIPE_ERR_EID                   7
-#define CSP_EVENT_COUNTS                   8
+#define CSP_RESERVED_EID              	0
+#define CSP_STARTUP_INF_EID           	1
+#define CSP_COMMAND_ERR_EID           	2
+#define CSP_COMMANDNOP_INF_EID        	3
+#define CSP_COMMANDRST_INF_EID        	4
+#define CSP_INVALID_MSGID_ERR_EID     	5
+#define CSP_LEN_ERR_EID               	6
+#define CSP_PIPE_ERR_EID              	7
+#define CSP_REGISTERCHILD_ERR_EID     	8
+#define CSP_SOCKET_ERR_EID            	9
+#define CSP_BIND_ERR_EID              	10
+#define CSP_BUFFERGET_ERR_EID         	11
+#define CSP_SENDTO_ERR_EID            	12
+#define CSP_RECVFROM_ERR_EID          	13
+#define CSP_EVENT_COUNTS              	14
+
 
 #endif /* _csp_app_events_h_ */
 
