185db99ed3a6905ab4c9e5d8f84c4f3bd0835cd8
Stephen <stephen99scott@gmail.com>
2020-09-14 20:07:31 -0400

    Made unique msgids

--- apps/csp_app/fsw/platform_inc/csp_app_msgids.h
+++ apps/csp_app/fsw/platform_inc/csp_app_msgids.h
@@ -12,9 +12,9 @@
 #ifndef _csp_app_msgids_h_
 #define _csp_app_msgids_h_
 
-#define CSP_APP_CMD_MID            	0x1882
-#define CSP_APP_SEND_HK_MID        	0x1883
-#define CSP_APP_HK_TLM_MID		    0x0883
+#define CSP_APP_CMD_MID            	0x1930
+#define CSP_APP_SEND_HK_MID        	0x1931
+#define CSP_APP_HK_TLM_MID		    0x0930
 
 #endif /* _csp_app_msgids_h_ */
 
